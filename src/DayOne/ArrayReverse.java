66d829a2a987173c81b2ea76a375de964ad68c37
Nicklas Di Pietro <Nicklasdipietro@gmail.com>
2018-07-10 17:45:15 -0700

    New Package and stuff sorted.

--- src/DayOne/ArrayReverse.java
+++ src/DayOne/ArrayReverse.java
@@ -1,4 +1,4 @@
-public class D01_ArrayReverse {
+public class ArrayReverse {
     public static void doubler(int [] arr) {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = arr[i] * 2;
--- src/DayOne/ArrayReverseTest.java
+++ src/DayOne/ArrayReverseTest.java
@@ -2,12 +2,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class D01_ArrayReverseTest {
+class ArrayReverseTest {
 
     @Test
     public void testEmpty() throws Exception {
         int[] numbers = {};
-        D01_ArrayReverse.doubler(numbers);
+        ArrayReverse.doubler(numbers);
 
         int[] expected = {};
         assertArrayEquals(expected, numbers);
@@ -16,7 +16,7 @@ class D01_ArrayReverseTest {
     @Test
     public void testDoubler() throws Exception {
         int[] numbers = {2, 4, 6, 8, 10};
-        D01_ArrayReverse.doubler(numbers);
+        ArrayReverse.doubler(numbers);
 
         int[] expected = {4, 8, 12, 16, 20};
         assertArrayEquals(expected, numbers);
@@ -25,7 +25,7 @@ class D01_ArrayReverseTest {
     @Test
     public void testReverse() throws Exception {
         int[] numbers = {10, 8, 6, 4, 2};
-        D01_ArrayReverse.reverse(numbers);
+        ArrayReverse.reverse(numbers);
 
         int[] expected = {2, 4, 6, 8, 10};
         assertArrayEquals(expected, numbers);
