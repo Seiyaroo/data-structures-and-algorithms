91ee2f7caba3fe12b7dd54e0bbeadc999d525e26
Nicklas Di Pietro <Nicklasdipietro@gmail.com>
2018-07-18 10:22:20 -0700

    Having a huge brain hurdle and can't get anything done. Need some TA or tutor help because I feel this quite often lately.

--- src/D07/KthElement.java
+++ src/D07/KthElement.java
@@ -1,20 +1,31 @@
 package D07;
 
+import D06.ListNode;
+
 import java.util.List;
 
 public class KthElement {
+    private ListNode root;
+
     public static void main(String[] args) {
+    }
 
+
+    public ListNode kthElementFromEnd(int k) {
+        ListNode current = this.root;
+// Get the total of the elements in the list
+        int total = this.size();
+// Declare a new Index
+        int newIndex = (total - k) - 1;
+// Get the element of newIndex from the List
+        this.get(newIndex);
+        return current;
+    }
+
+    private void get(int newIndex) {
+    }
+
+    private int size() {
+        return 0;
     }
 }
-
-public class ListNode {
-    public int data;
-    public ListNode next;
-
-    public ListNode (int data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
