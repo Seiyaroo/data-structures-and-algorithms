9fe7f10a9738d8c9e0b124f735843ccf306e193b
Nicklas Di Pietro <Nicklasdipietro@gmail.com>
2018-07-18 09:42:42 -0700

    Base model.

---
+++ src/D07/KthElement.java
@@ -0,0 +1,20 @@
+package D07;
+
+import java.util.List;
+
+public class KthElement {
+    public static void main(String[] args) {
+
+    }
+}
+
+public class ListNode {
+    public int data;
+    public ListNode next;
+
+    public ListNode (int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
