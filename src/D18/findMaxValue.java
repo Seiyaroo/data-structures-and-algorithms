74eb05d1ac1a17eb2b1f0b48d9c4c6c36efe05d6
Nicklas Di Pietro <Nicklasdipietro@gmail.com>
2018-08-02 11:00:41 -0700

    Changed file names, finished up some code. I think its working now?

--- src/D18/findMaxValue.java
+++ src/D18/findMaxValue.java
@@ -1,29 +1,28 @@
 package D18;
 
+import D16.TreeNode;
 
-import javax.swing.tree.TreeNode;
 import java.util.LinkedList;
 import java.util.Queue;
-
 public class findMaxValue {
-    public static void main(String[] args) {
-
-    }
-
-    public static void findMax(TreeNode root) {
-        TreeNode root;
+    public static int findMax(TreeNode root) {
         int maximumValue = 0;
 
         //create a queue
         Queue<TreeNode> breadthQueue = new LinkedList<>();
+
         //add the root outside the while loop
         breadthQueue.add(root);
 
-        System.out.println(breadthQueue);
-        System.out.println(root);
-
         while (breadthQueue != null) {
             TreeNode temp = breadthQueue.poll();
+
+            //compares the placeholder maxVal as 0 and if the new highest encountered val is > than that
+            // then you can say the new value is the max value currently. Until another is encountered.
+            if (temp.data > maximumValue) {
+                maximumValue = temp.data;
+            }
+
             System.out.println("Visited Node: " + temp);
 
             if (temp.left != null) {
@@ -34,6 +33,7 @@ public class findMaxValue {
                 breadthQueue.add(temp.right);
             }
         }
+        return maximumValue;
     }
 }
 
