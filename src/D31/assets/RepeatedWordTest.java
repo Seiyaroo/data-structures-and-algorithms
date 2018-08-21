07b986e579c7b1a06ebc7eb97fc70b56af0b7d30
Nicklas Di Pietro <Nicklasdipietro@gmail.com>
2018-08-21 14:09:33 -0700

    Fixing stuff.

--- src/D31/assets/RepeatedWordTest.java
+++ src/D31/assets/RepeatedWordTest.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-class RepeatedWordTest {
+public class RepeatedWordTest {
 
     @Test
-    void emptyStringTest() {
+    public void emptyStringTest() {
         String story = "";
 
         String expected = "";
@@ -20,7 +20,7 @@ class RepeatedWordTest {
     }
 
     @Test
-    void singleWordStringTest() {
+    public void singleWordStringTest() {
         String story = "Single";
 
         String expected = "";
@@ -33,7 +33,7 @@ class RepeatedWordTest {
     }
 
     @Test
-    void onceUponStringTest() {
+    public void onceUponStringTest() {
         String story = "Once upon a time, there was a brave princess who...";
 
         String expected = "a";
@@ -46,7 +46,7 @@ class RepeatedWordTest {
     }
 
     @Test
-    void noRepeatsStringTest() {
+    public void noRepeatsStringTest() {
         String story = "Once upon a time, there was brave princess who...";
 
         String expected = "";
@@ -59,7 +59,7 @@ class RepeatedWordTest {
     }
 
     @Test
-    void bestOfTimesTest() {
+    public void bestOfTimesTest() {
         String story = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...";
 
         String expected = "it";
@@ -72,7 +72,7 @@ class RepeatedWordTest {
     }
 
     @Test
-    void summerTest() {
+    public void summerTest() {
         String story = "It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York...";
 
         String expected = "summer";
@@ -85,7 +85,7 @@ class RepeatedWordTest {
     }
 
     @Test
-    void stringWithNumbersTest() {
+    public void stringWithNumbersTest() {
         String story = "I am 15 years and 3 months and 2 days.";
 
         String expected = "and";
@@ -98,7 +98,7 @@ class RepeatedWordTest {
     }
 
     @Test
-    void onlyNumbersTest() {
+    public void onlyNumbersTest() {
         String story = "1 2 3, 4 5 6 7, 8 9 10 11 12... 237";
 
         String expected = "";
