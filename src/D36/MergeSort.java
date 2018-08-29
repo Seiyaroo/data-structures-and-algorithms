239e2700fb3d2955a8bd70dfebe43838431591b9
Nicklas Di Pietro <Nicklasdipietro@gmail.com>
2018-08-29 13:51:29 -0700

    Working on finishing up.

--- src/D36/MergeSort.java
+++ src/D36/MergeSort.java
@@ -3,24 +3,24 @@ package D36;
 //Crediting Geeks for Geeks
 
 /* Java program for Merge Sort */
-class MergeSort {
+public class MergeSort {
     // Merges two subarrays of arr[]. 
     // First subarray is arr[l..m] 
     // Second subarray is arr[m+1..r] 
-    void merge(int arr[], int l, int m, int r) {
+    void mergeSort(int array[], int left, int middle, int right) {
         // Find sizes of two subarrays to be merged 
-        int n1 = m - l + 1;
-        int n2 = r - m;
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
 
         /* Create temp arrays */
-        int L[] = new int [n1];
-        int R[] = new int [n2];
+        int Left[] = new int[n1];
+        int Right[] = new int[n2];
 
         /*Copy data to temp arrays*/
-        for (int i=0; i<n1; ++i)
-            L[i] = arr[l + i];
-        for (int j=0; j<n2; ++j)
-            R[j] = arr[m + 1+ j];
+        for (int i = 0; i < n1; ++i)
+            Left[i] = array[left + i];
+        for (int j = 0; j < n2; ++j)
+            Right[j] = array[middle + 1 + j];
 
 
         /* Merge the temp arrays */
@@ -29,45 +29,46 @@ class MergeSort {
         int i = 0, j = 0;
 
         // Initial index of merged subarry array 
-        int k = l;
+        int k = left;
         while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
+            if (Left[i] <= Right[j]) {
+                array[k] = Left[i];
                 i++;
             } else {
-                arr[k] = R[j];
+                array[k] = Right[j];
                 j++;
             }
             k++;
         }
 
-        /* Copy remaining elements of L[] if any */
+        /* Copy remaining elements of Left[] if any */
         while (i < n1) {
-            arr[k] = L[i];
+            array[k] = Left[i];
             i++;
             k++;
         }
 
-        /* Copy remaining elements of R[] if any */
+        /* Copy remaining elements of Right[] if any */
         while (j < n2) {
-            arr[k] = R[j];
+            array[k] = Right[j];
             j++;
             k++;
         }
     }
 
-    // Main function that sorts arr[l..r] using 
+    // Main function that sorts arr[l..r] using
     // mergeSort()
     void sort(int arr[], int l, int r) {
         if (l < r) {
             // Find the middle point 
-            int m = (l+r)/2;
+            int m = (l + r) / 2;
 
             // Sort first and second halves 
             sort(arr, l, m);
-            sort(arr , m+1, r);
+            sort(arr, m + 1, r);
 
             // Merge the sorted halves 
-            merge(arr, l, m, r);
+            mergeSort(arr, l, m, r);
         }
-    } 
+    }
+}
--- src/D37/SelectionSort.java
+++ src/D37/SelectionSort.java
@@ -1,157 +1,111 @@
 package D37;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class SelectionSort {
-//    // Selection sort method
-//    public static void selectionSort(int array[]) {
-//        int n = array.length;
-//
-//        //Iterate through the array via loop
-//        for (int i = 0; i < n-1; i++) {
-//            //Try to find the minimum element in the array
-//            int min = i;
-//            for (int j = i+1; j < n; j++)
-//                if (array[j] < array[min]) {
-//                    min = j;
-//                }
-//        }
-//        //Swapping mechanism to swap the min int with the first index in the array
-//        int temp = array[min];
-//        array[min] = array[i];
-//        array[i] = temp;
-//    }
+    // Selection sort method
+    public static double[] selectionSort(double[] input) {
+        //Iterate through the array via loop
+        for (int i = 0; i < input.length; i++) {
+            //Try to find the minimum element in the array
+            int min = i;
+
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[j] < input[min]) {
+                    min = j;
+                }
+            }
+        //Swapping mechanism to swap the min int with the first index in the array
+        double tempStorage = input[i];
+        input[i] = input[min];
+        input[min] = tempStorage;
+    }
+    return input;
+}
 
     //Merge sort method
-    public static void mergeSort(int arr[], int x, int y, int z)
-    {
+    void mergeSort(double array[], int left, int middle, int right) {
         // Find sizes of two subarrays to be merged
-        int n1 = y - x + 1;
-        int n2 = z - y;
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
 
         /* Create temp arrays */
-        int Left[] = new int [n1];
-        int Right[] = new int [n2];
+        double Left[] = new double[][n1];
+        double Right[] = new double[][n2];
 
         /*Copy data to temp arrays*/
-        for (int i=0; i<n1; ++i)
-            Left[i] = arr[x + i];
-        for (int j=0; j<n2; ++j)
-            Right[j] = arr[y + 1+ j];
-        System.out.println("" + Arrays.toString(arr));
+        for (int i = 0; i < n1; ++i)
+            Left[i] = array[left + i];
+        for (int j = 0; j < n2; ++j)
+            Right[j] = array[middle + 1 + j];
+
+
         /* Merge the temp arrays */
 
         // Initial indexes of first and second subarrays
         int i = 0, j = 0;
 
-        // Initial index of merged subarray
-        int k = x;
-        while (i < n1 && j < n2)
-        {
-            if (Left[i] <= Right[j])
-            {
-                arr[k] = Left[i];
+        // Initial index of merged subarry array
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (Left[i] <= Right[j]) {
+                array[k] = Left[i];
                 i++;
-            }
-            else
-            {
-                arr[k] = Right[j];
+            } else {
+                array[k] = Right[j];
                 j++;
             }
             k++;
         }
 
         /* Copy remaining elements of Left[] if any */
-        while (i < n1)
-        {
-            arr[k] = Left[i];
+        while (i < n1) {
+            array[k] = Left[i];
             i++;
             k++;
         }
 
         /* Copy remaining elements of Right[] if any */
-        while (j < n2)
-        {
-            arr[k] = Right[j];
+        while (j < n2) {
+            array[k] = Right[j];
             j++;
             k++;
         }
-        System.out.println("" + Arrays.toString(arr));
     }
 
-    // Main function that sorts arr[Left..Right] using
+    // Main function that sorts arr[l..r] using
     // mergeSort()
-    public static void sort(int arr[], int l, int r)
-    {
-        if (l < r)
-        {
+    public void sort(double[] inputArr, int left, int right) {
+        if (left < right) {
             // Find the middle point
-            int m = (l+r)/2;
+            int middle = (left + right) / 2;
 
             // Sort first and second halves
-            sort(arr, l, m);
-            sort(arr , m+1, r);
+            sort(inputArr, left, middle);
+            sort(inputArr, middle + 1, right);
 
             // Merge the sorted halves
-            mergeSort(arr, l, m, r);
+            mergeSort(inputArr, left, middle, right);
         }
     }
-
     // ==============HELPER===============
     // ==============FUNCTIONS============
 
-//    public static void printTheArray(int array[]) {
-//        int n = array.length;
-//        for (int i = 0; i < n; i++) {
-//            System.out.print(array[i] + " ");
-//            System.out.println();
-//        }
-//    }
-//
-//    public static int isSorted(int array[], int n) {
-//        if (n == 0 || n == 1) {
-//            return 1;
-//        }
-//        if (array[n-1] < array[n-2]) {
-//            return 0;
-//        }
-//        return isSorted(array, n-1);
-//    }
-        public static int[] generateRandomSizedArray (int size) {
 
-            int[] a = new int[size];
-
-            for (int i = 0; i < size; i++) {
-                a[i] = (int)(Math.random()*10);
-
-                for (int j = 0; j < i; j++) {
-                    if (a[i] == a[j]) {
-                        a[j] = (int)(Math.random()*10);
-                    }
-                }
+    public static double[] randomN (int size) {
+        double[] randomData = new double[size];
+        for (int i = 0; i < size; i++) {
+            Random randomVal = new Random();
+            randomData[i] = randomVal.nextDouble() * 10000;
             }
-
-            for (int i = 0; i < a.length; i++) {
-                System.out.print(a[i]+" ");
-            }
-            System.out.println();
-            return a;
+            return randomData;
         }
-//
-//    public static Random shuffleTheArray(int[] arrayShuffle) {
-//        Random rnd = ThreadLocalRandom.current();
-//        for (int i = arrayShuffle.length - 1; i > 0; i--) {
-//            int index = arrayShuffle(Math.random());
-//
-//            int a = arrayShuffle[index];
-//            arrayShuffle[index] = arrayShuffle[i];
-//            arrayShuffle[i] = a;
-//        }
-//    }
+    }
 
+    public static double[] duplicator(double[] original) {
+        double[] copy = new double[original.length];
 
+        for (int i = 0; i < size; i++)
+    }
 
-}
